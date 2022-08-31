0f71044e45bbe3e0fead94b84a1ff2355d1d38b1
DreVinciBot <35714752+DreVinciCode@users.noreply.github.com>
2022-08-31 16:33:48 -0400

    Added 3rd dimension control

--- Assets/Scripts/HandMirrorPublisher.cs
+++ Assets/Scripts/HandMirrorPublisher.cs
@@ -14,13 +14,17 @@ namespace RosSharp.RosBridgeClient
 
         public bool _publishMessageCheck { get; set; }
         public bool _objectDetected { get; set; }
+        public bool x_fixed { get; set; }
+        public bool y_fixed { get; set; }
+        public bool z_fixed { get; set; }
+
 
         private MessageTypes.Geometry.Twist message;
         private float _linearX = 0;
         private float _linearY = 0;
         private float _linearZ = 0;
         private float _distanceThreshold = 0.01f;
-        private float _forwardSeparation = 0.1f;
+        private float _forwardSeparation = 0.2f;
         private float _vertialOffset = 0.1872f;
         private float left_thumbCurl;
         private float left_indexCurl;
@@ -32,6 +36,7 @@ namespace RosSharp.RosBridgeClient
 
         private bool _activeState = false;
         private bool _stopChecked = false;
+        private bool _sendZeros = true;
 
         private MixedRealityPose _leftThumbPose, _leftPalmPose, _leftIndexPose, _leftMiddlePose, _rightThumbPose, _rightIndexPose;
 
@@ -53,13 +58,23 @@ namespace RosSharp.RosBridgeClient
 
         private void PublishMessage()
         {
+            if (!x_fixed)
+                _linearX = 0;
+
+            if (!y_fixed)
+                _linearY = 0;
+
+            if (!z_fixed)
+                _linearZ = 0;
+
             Vector3 linearVelocity = new Vector3(_linearX, _linearY, _linearZ);
             Vector3 angularVelocity = new Vector3(0f, 0f, 0f);
 
             message.linear = GetGeometryVector3(linearVelocity);
             message.angular = GetGeometryVector3(angularVelocity);
 
-            Publish(message);
+            if(_publishMessageCheck)
+                Publish(message);
         }
 
         private void Update()
@@ -72,7 +87,7 @@ namespace RosSharp.RosBridgeClient
 
             _leftIndexObject.GetComponent<Renderer>().enabled = false;
 
-
+            //Right hand trigger
             if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out _rightIndexPose) &&
                 HandJointUtils.TryGetJointPose(TrackedHandJoint.ThumbTip, Handedness.Right, out _rightThumbPose))
             {
@@ -90,60 +105,65 @@ namespace RosSharp.RosBridgeClient
             else
             {
                 _activeState = false;
-                Status.text = "Active";
+                Status.text = "Inactive";
             }
 
+
+            // Send Zero velocity if any of these conditions occur
+            //
+
+            // If right trigger not detected or target image not detected
             if (!_activeState || !_objectDetected)
             {
                 _linearX = 0;
                 _linearY = 0;
                 _linearZ = 0;
-                Direction.text = "--";
+                PublishMessage();
+                return;
             }
-            else if (left_indexCurl < 0.1f && left_middleCurl < 0.1f && left_ringCurl < 0.1f && left_pinkyCurl < 0.1f && left_thumbCurl < 0.4f)
+
+            /*
+            //if (left_indexCurl < 0.1f && left_middleCurl < 0.1f && left_ringCurl < 0.1f && left_pinkyCurl < 0.1f && left_thumbCurl < 0.4f)
             {
-                //Call the stop command?
                 _linearX = 0;
                 _linearY = 0;
                 _linearZ = 0;
-                Direction.text = "Stop";
-                _stopChecked = true;
+                PublishMessage();
+                return;
             }
-            else if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Left, out _leftPalmPose) &&
-                HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Left, out _leftIndexPose))
+            */
+
+            if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Left, out _leftPalmPose) &&
+                HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Left, out _leftIndexPose) &&
+                HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out _rightIndexPose))
             {
                 _leftIndexObject.transform.position = _leftIndexPose.Position;
-
-                //var test = _leftIndexPose.Position.
                 _leftIndexObject.GetComponent<Renderer>().enabled = true;
-
 
                 //Match the height here. Kinova arm up/down is Z, Unity is Y
                 var z_arm_difference = (_leftPalmPose.Position.y - Target_EndEffectorImage.transform.position.y) + _vertialOffset;
-                var y_arm_difference = _leftIndexPose.Position.x - Target_EndEffectorImage.transform.position.x;
-                var x_arm_difference = _leftIndexPose.Position.z - Target_EndEffectorImage.transform.position.z;
-
-
-                Debug.Log("z difference: " + z_arm_difference + " x difference: " + x_arm_difference);
+                var y_arm_difference = _leftIndexPose.Position.x - Target_EndEffectorImage.transform.position.x;                   
+                //var x_arm_difference = (_leftIndexPose.Position.z - Target_EndEffectorImage.transform.position.z) - _forwardSeparation;
+                var x_arm_difference = -(_leftIndexPose.Position.z - _rightIndexPose.Position.z);
 
                 if (Mathf.Abs( z_arm_difference) > _distanceThreshold)
                 {
                     _linearZ = z_arm_difference;
-                    _linearX = 0;
+                    _linearX = x_arm_difference;
                     _linearY = y_arm_difference;
                     Direction.text = (Mathf.Round((z_arm_difference) * 100f) / 100f).ToString();
                 }
+
+                PublishMessage();
             }
             else
             {
                 _linearX = 0;
                 _linearY = 0;
                 _linearZ = 0;
-                Direction.text = "--";
-            }
-
-            if (_publishMessageCheck)
                 PublishMessage();
+                return;
+            }
         }
 
         private static MessageTypes.Geometry.Vector3 GetGeometryVector3(Vector3 vector3)
